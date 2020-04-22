9075d2e6e8226c0f4fba2462bd521ee4f796386e
Lucas Easley <lucaseasley11@gmail.com>
2020-04-22 13:48:23 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,50 +1,52 @@
-  # Define your Song class
 class Song
-    @@count = 0
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count +=1
+    @@genres << genre
+    @@artists << artist
+  end
 
   def self.count
     @@count
   end
-    # returns an array of all of the genres of existing songs.
-    # array should contain only unique genres
-  @@genres = []
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
+  end
 
   def self.genres
     @@genres.uniq
   end
 
-  def self.artist_count
-      @@artists.inject(Hash.new(0)) { |total, i| total[i] += 1 ;total}
-      #When running inject on a Hash, the hash is first converted to an array
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1
+      else
+        genre_count[genre] = 1
+      end
     end
-
-   def self.genre_count
-     genre_count = {}
-     @@genres.each do |genre|
-       if genre_count[genre]
-         genre_count[genre] +=  1
-       else
-         genre_count[genre] = 1
-       end
-     end
-     genre_count
-   end
-
-  @@artists = []
-
-
-
-    def self.artists
-      @@artists.uniq
-    end
-
-    def initialize(name, artist, genre)
-      @name = name
-      @artist = artist
-      @@artists << artist
-      @genre = genre
-      @@genres << genre
-      @@count += 1
-    end
-    attr_accessor :name, :artist, :genre
+    genre_count
   end
+end
